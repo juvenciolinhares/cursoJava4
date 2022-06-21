8acdbbc48ec62f739ba215c857b2b823747f8316
juvenciolinhares <jlinhares@cpqi.com>
2022-06-21 09:28:29 -0300

    math

---
+++ math/MathTest.java
@@ -0,0 +1,32 @@
+package math;
+
+public class MathTest {
+
+	public static void main(String[] args) {
+
+		// relizar calculos chamando os métodos estáticos da classe:
+
+		// seno:
+		System.out.println(Math.sin(10));
+
+		// cosseno:
+		System.out.println(Math.cos(10));
+
+		// tangente
+		System.out.println(Math.tan(45));
+
+		// potenciação:
+		System.out.println(Math.pow(4, 3));
+
+		// raiz quadrada:
+		System.out.println(Math.sqrt(16));
+
+		// razia cubica:
+		System.out.println(Math.cbrt(16));
+
+		// hipotenuza:
+		System.out.println(Math.hypot(6, 8));
+
+	}
+
+}
