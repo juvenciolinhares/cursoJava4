5f6bdc0f8e5ec04c756fe7b8dc9795c66bdef23c
juvenciolinhares <jlinhares@cpqi.com>
2022-06-21 09:36:39 -0300

    math atualizacao

--- math/MathTest.java
+++ math/MathTest.java
@@ -30,3 +30,27 @@ public class MathTest {
 	}
 
 }
+
+/*alguns métodos:
+ * sin() seno
+ * cos()cosseno
+ * tan() tangente
+ * asin() arco seno de v(intervalo de -1.0 a 1.0)
+ * acos()arco cosseno de v(intervalo de -1.0 a 1.0)
+ * atan()arco tangente de v(intervalo de -pi/2 a pi/2)
+ * exp()eule elevado a x
+ * sinh()seno hiperbólico de x
+ * cosh()cosseno hiperbólico de x
+ * tanh()tangente hiperbólico de x
+ * pow() y elevado a x
+ * log()logaritmo natural de x
+ * sqrt() raiz quadrada de x
+ * cbrt()raiz cúbica de x
+ * singnum()1 se x positivo, -1 se x negativo e 0 se x=0
+ * ceil()maior inteiro proximo de x
+ * floor() menor numero interiro menor ou igual a x
+ * round()numero arredondado de x
+ * max()maior de dois numexo
+ * min()menor de dois numeros
+ * hypot()comprimento da hyp de um triangulo retangulo de lados x e y
+ */
